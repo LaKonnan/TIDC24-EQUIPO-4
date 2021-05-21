fd3448064c664d38624fc3b361bcc23362140c36
rboockv <rudyboockv@gmail.com>
2021-05-20 23:11:24 -0400

    tests añadidos y endpoint obraapi modificado

--- frontend/src/axios-api.js
+++ frontend/src/axios-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const getAPI = axios.create({
-    baseURL: 'https://xlpxbsnpo3.execute-api.us-east-2.amazonaws.com/dev/'
+    baseURL: 'https://ee7rryknn1.execute-api.us-east-2.amazonaws.com/dev'
 })
 
 export { getAPI }
