c792bea7e1c9f14ad09a11c649673a990582d481
Konnan <wcamposses@gmail.com>
2021-06-08 19:41:55 -0400

    Actualizacion cajas chicas

--- frontend/src/components/cajas/DialogEdit.vue
+++ frontend/src/components/cajas/DialogEdit.vue
@@ -1,40 +1,38 @@
 <template>
   <div>
       <b-modal id="modal-edit" size="xl" title="EDITAR CAJA CHICA" hide-footer>
-          <b-col>
-              <b-row>ID</b-row>
-              <b-row>O556-02</b-row>
-          </b-col>
-
-          <b-col>
-              <b-row>Tipo</b-row>
-              <b-row>Obra</b-row>
-          </b-col>
-
-          <b-col>
-              <b-row>Estado</b-row>
-              <b-row> </b-row>
-          </b-col>
-
-          <b-col>
-              <b-row>Monto de gastos</b-row>
-              <b-row>145700</b-row>
-          </b-col>
-
-          <b-col>
-              <b-row>Monto total</b-row>
-              <b-row>180000</b-row>
-          </b-col>
-
-          <b-col>
-              <b-row>Fecha de inicio </b-row>
-              <b-row> </b-row>
-          </b-col>
-
-          <b-col>
-              <b-row>Fecha de término</b-row>
-              <b-row> </b-row>
-          </b-col>     
+          <b-row>
+              <b-col></b-col>
+              <b-col></b-col>
+          </b-row>
+          <b-row>
+              <b-col></b-col>
+              <b-col></b-col>
+          </b-row>
+          <b-row>
+              <b-col></b-col>
+              <b-col></b-col>
+          </b-row>
+          <b-row>
+              <b-col></b-col>
+              <b-col></b-col>
+          </b-row>
+          <b-row>
+              <b-col></b-col>
+              <b-col></b-col>
+          </b-row>
+          <b-row>
+              <b-col></b-col>
+              <b-col></b-col>
+          </b-row>
+          <b-row>
+              <b-col></b-col>
+              <b-col></b-col>
+          </b-row>
+          <b-row>
+              <b-col></b-col>
+              <b-col></b-col>
+          </b-row>  
       </b-modal>
   </div>
 </template>
